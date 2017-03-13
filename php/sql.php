4b4d696fe3b61e0a3e53be58101c427c6460c09f
Nkosinathi Khumalo <Nkosinathi.Khumalo@investec.co.za>
2017-03-13 08:39:59 +0200

    updated sql - search

--- php/sql.php
+++ php/sql.php
@@ -1,87 +1,135 @@
 <?php
 
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
+
+
 /**
  * Description of sql
  *
  * @author Nkosinathi.Khumalo
  */
+
 class sqlClass {
-
-    private function connectPDO() {
-
-        $connection = null;
-
-        $servername = "localhost";
-        $username = "root";
-        $password = "";
-        $dbname = "drs";
-
-        /*   $servername = "154.0.173.140";
+	
+	
+	private function connectPDO() {
+		
+		
+		$connection = null;
+		
+		
+		$servername = "localhost";
+		
+		$username = "root";
+		
+		$password = "";
+		
+		$dbname = "drs";
+		
+		
+		
+		/*   $servername = "154.0.173.140";
           $username = "webdenpp_admin";
           $password = "Bhung@ne002";
           $dbname = "webdenpp_leecakes"; */
-        $s = "";
+		
+		$s = "";
+		
+		
+		try {
+			
+			
+			// 			Create connection
+			$connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+			
+			
+			// 			set the PDO error mode to exception
+			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			
+		}
+		catch (PDOException $e) {
+			
+			
+			die($e->getMessage());
+			
+		}
+		
+		
+		return $connection;
+		
+	}
+	
+	
+	private function query_read($q) {
+		
+		
+		$query = "";
+		
+		
+		switch ($q) {
+			
+			
+			case "authenticate":
+			
+			$query = "SELECT `PASSWORD`, `USERID`, `USERNAME` FROM `logins` WHERE `USERNAME` = ?";
+			
+			
+			break;
+			
+			case "authenticate_getUser":
+			$query = "SELECT `USERID` FROM `logins` WHERE `USERNAME` = ?";
+			
+			break;
+			
+			case "getProfile":
+			
+			$query = "SELECT * FROM `users` WHERE `LOGIN` = ?";
+			
+			
+			break;
+			
+			
+			case "searchProfile":
+			$query = "SELECT * FROM `users` WHERE (CONVERT(`CARDNUMBER` USING utf8) LIKE ? " ." '%') OR (CONVERT(`CARDNUMBER` USING utf8) LIKE ?". "'%')"; //OR (CONVERT(`IDNUMBER` USING utf8) LIKE '?')";
 
-        try {
-
-            // Create connection
-            $connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-
-            // set the PDO error mode to exception
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) {
-
-            die($e->getMessage());
-        }
-
-        return $connection;
-    }
-
-    private function query_read($q) {
-
-        $query = "";
-
-        switch ($q) {
-
-            case "authenticate":
-
-                $query = "SELECT `PASSWORD`, `USERID`, `USERNAME` FROM `logins` WHERE `USERNAME` = ?";
-
-                break;
-            case "authenticate_getUser":
-                $query = "SELECT `USERID` FROM `logins` WHERE `USERNAME` = ?";
-                break;
-            case "getProfile":
-
-                $query = "SELECT * FROM `users` WHERE `LOGIN` = ?";
-
-                break;
-
-            case "searchProfile":
-                    $query = "SELECT * FROM `users` WHERE `CARDNUMBER` LIKE = ? OR `IDNUMBER` LIKE  ?";
-                break;
-        }
-
-        return $query;
-    }
-
-    private function query_insert($q) {
-
-        $query = "";
-
-        switch ($q) {
-            case "addProduct":
-                $query = "INSERT INTO `products`(`ID`, `TITLE`, `DESCRIPTION`, `PRICE`, `IMG`, `CATEGORY`) VALUES (?,?,?,?,?,?)";
-            case "addProduct_images":
-                $query = "INSERT INTO `products_images`(`ID`, `IMG`, `PRODUCTID`, `TYPE`) VALUES (?,?,?,?)";
-                break;
-        }//"SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '?????)' at line 1Array(    [0] => 42000    [1] => 1064    [2] => You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '?????)' at line 1)null"
+            //(CONVERT(`CARDNUMBER` USING utf8) LIKE '%?%') OR (CONVERT(`IDNUMBER` USING utf8) LIKE '%?%')
+			
+			break;
+			
+		}
+		
+		
+		return $query;
+		
+	}
+	
+	
+	private function query_insert($q) {
+		
+		
+		$query = "";
+		
+		
+		switch ($q) {
+			
+			case "addProduct":
+			$query = "INSERT INTO `products`(`ID`, `TITLE`, `DESCRIPTION`, `PRICE`, `IMG`, `CATEGORY`) VALUES (?,?,?,?,?,?)";
+			
+			case "addProduct_images":
+			$query = "INSERT INTO `products_images`(`ID`, `IMG`, `PRODUCTID`, `TYPE`) VALUES (?,?,?,?)";
+			
+			break;
+			
+		}
+		//"		SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax;
+	//	check the manual that corresponds to your MySQL server version for the right syntax to use near '?????)' at line 1Array(    [0] => 42000    [1] => 1064    [2] => You have an error in your SQL syntax;
+	//	check the manual that corresponds to your MySQL server version for the right syntax to use near '?????)' at line 1)null"
 
         return $query;
     }
@@ -251,7 +299,7 @@ class sqlClass {
             $stmt = $connect->prepare($query);
 
             $stmt->bindParam(1, $userID, PDO::PARAM_INT);
-
+     
             $stmt->execute();
 
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -269,25 +317,28 @@ class sqlClass {
     public function searchProfile($data) {
 
         $connect = $this->connectPDO();
-        $userKey = $data['search_key'] . "%";
+        $userKey = $data['search_key'];
         
         //check connection
         if ($connect != null) {
 
             $query = $this->query_read('searchProfile');
-//die($query);
+
             $stmt = $connect->prepare($query);
 
-            $stmt->bindParam(1, $userKey, PDO::PARAM_STR);
-             $stmt->bindParam(2, $userKey, PDO::PARAM_STR);
-            print_r($stmt['queryString']);
+            $stmt->bindParam(1, $userKey , PDO::PARAM_STR);
+            $stmt->bindParam(2,$userKey, PDO::PARAM_STR);
+
             $stmt->execute();
 
             $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
             if ($stmt->rowCount() > 0) {
 
-                echo json_encode(array("status" => 200, 'message' => 'success', 'data' => $result));
+                
+                return json_encode(array("status" => 200, 'message' => 'success', 'data' => $result));
+            } else {
+return json_encode(array("status" => 400, 'message' => 'fail', 'data' => null));
             }
         } else {
 
@@ -374,7 +425,8 @@ class sqlClass {
             $stmt->bindParam(6, $data['category'], PDO::PARAM_STR);
 
             $stmt->execute();
-            //"SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '?????)' at line 1Array(    [0] => 42000    [1] => 1064    [2] => You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '?????)' at line 1)null" 
+            //"SQLSTATE[42000]: Syntax error or access violation: 1064 You have an error in your SQL syntax;
+	//
 
             if ($stmt) {
 
