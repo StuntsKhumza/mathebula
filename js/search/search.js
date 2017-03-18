56bd9d1237ab380dcba45a30b346d1a2f597b98f
Nkosinathi Khumalo <Nkosinathi Khumalo>
2017-03-18 18:49:15 +0200

    added que

--- js/search/search.js
+++ js/search/search.js
@@ -9,7 +9,8 @@ angular.module('search-app', ['session-app', 'ngCookies'])
 
                 $scope.searching = false;
                 var self = this;
-
+                self.showQueue = false;
+                self.queue = []
                 self.showmsg = "";
 
                 self.searchObj = {
@@ -22,72 +23,87 @@ angular.module('search-app', ['session-app', 'ngCookies'])
                 $scope.results = [
                 ]
 
-                var getHistory = $cookies.get('dr_search_history');
+                var getHistory = getCookie_object('dr_search_history');//$cookies.get('dr_search_history');
 
-                if(getHistory != null){
-
-                    getHistory = b64DecodeUnicode(getHistory);
-
-                    $scope.results = JSON.parse(getHistory);
+                if (getHistory != null) {
+                    $scope.results = getHistory;
                 }
 
+                var dr_query =  getCookie_object('dr_queue');//self.queue
+
+                 if (dr_query != null) {
+                    self.queue = dr_query;
+                }
+
+
+                self.show_queue = function(){
+
+                    if(self.showQueue){
+self.showQueue = false;
+                    } else {
+                        self.showQueue = true;
+                    }
+
+                }
                 //load search 
 
                 $scope.search = function () {
                     var validate = validate_input();
 
-                    if (!validate.status){
+                    if (!validate.status) {
                         self.showmsg = validate.msg;
                         return;
                     } else {
                         self.showmsg = "";
                     }
-                   
+
                     var searchV = "";
                     var search_key_data;
                     var search_type = '';
-                  
-                        if (self.searchObj.file_number.length > 0) {
 
-                            searchV = "file number: '" + self.searchObj.file_number + "'";
-                            search_key_data = self.searchObj.file_number;
-                            search_type = "card";
-                        } else if (self.searchObj.id_number.length > 0) {
+                    if (self.searchObj.file_number.length > 0) {
 
-                            searchV = "id number: '" + self.searchObj.id_number + "'";
-                            search_key_data = self.searchObj.id_number;
-                            search_type = "id";
-                        }
+                        searchV = "file number: '" + self.searchObj.file_number + "'";
+                        search_key_data = self.searchObj.file_number;
+                        search_type = "card";
+                    } else if (self.searchObj.id_number.length > 0) {
 
-                        $scope.searching = true;
-                        self.search_complete = false;
+                        searchV = "id number: '" + self.searchObj.id_number + "'";
+                        search_key_data = self.searchObj.id_number;
+                        search_type = "id";
+                    }
 
-                        var search_data = { search_key: search_key_data, "search_type": search_type }
-                        var search_call = serviceSession.search_profile(search_data);
+                    $scope.searching = true;
+                    self.search_complete = false;
 
-                        search_call.then(function (res) {
+                    var search_data = { search_key: search_key_data, "search_type": search_type }
+                    var search_call = serviceSession.search_profile(search_data);
 
-                            self.showmsg2 = "Showing results for " + searchV;
-                            $scope.results = res.data;
-                      
-                            var str = b64EncodeUnicode(JSON.stringify(res.data));
-                            $cookies.put('dr_search_history', str);
-                         
-                            self.searchObj.file_number = "";
-                            self.searchObj.id_number = "";
-                            self.search_complete = true;
-                            $scope.searching = false;
-                        });
+                    search_call.then(function (res) {
 
-                    
+                        self.showmsg2 = "Showing results for " + searchV;
+                        $scope.results = res.data;
+
+                        writeCookie_object(res.data, 'dr_search_history');
+
+                        //var str = b64EncodeUnicode(JSON.stringify(res.data));
+                        //$cookies.put('dr_search_history', str);
+
+                        self.searchObj.file_number = "";
+                        self.searchObj.id_number = "";
+                        self.search_complete = true;
+                        $scope.searching = false;
+                    });
+
+
 
 
                     return;
-                   
+
 
                 }
 
-                function validate_input(){
+                function validate_input() {
 
                     var result = {
                         status: true,
@@ -95,24 +111,24 @@ angular.module('search-app', ['session-app', 'ngCookies'])
                     };
 
                     //check if filled in
-                    if(self.searchObj.file_number.length == 0 && self.searchObj.id_number.length == 0){
-                            result.status = false;
-                            result.msg = "Please ensure that you provide a File or ID Number below";
+                    if (self.searchObj.file_number.length == 0 && self.searchObj.id_number.length == 0) {
+                        result.status = false;
+                        result.msg = "Please ensure that you provide a File or ID Number below";
                     }
 
-                    
-                    if (self.searchObj.file_number.length > 0 && result.status == true){
 
-                        if(self.searchObj.file_number.length < 2){
+                    if (self.searchObj.file_number.length > 0 && result.status == true) {
+
+                        if (self.searchObj.file_number.length < 2) {
                             result.status = false;
                             result.msg = "Please ensure you provide at least 2 characters for File Search";
                         }
 
                     }
 
-                    if (self.searchObj.id_number.length > 0 && result.status == true){
+                    if (self.searchObj.id_number.length > 0 && result.status == true) {
 
-                        if(self.searchObj.id_number.length < 6){
+                        if (self.searchObj.id_number.length < 6) {
                             result.status = false;
                             result.msg = "Please ensure you provide 6 digit ID Number";
                         }
@@ -141,8 +157,27 @@ angular.module('search-app', ['session-app', 'ngCookies'])
                         $scope.userObj.client = o;
                         $scope.userObj.clientSet = true;
                     }
-                
 
+
+                }
+
+                self.addToQue = function (id) {
+
+                    //check if added
+                    var o = find_Item(self.queue, id);
+
+                    if (o == null) {
+
+
+                        var o = find_Item($scope.results, id);
+
+                        if (o != null) {
+                            self.queue.push(o);
+                        }
+                    }
+
+                     writeCookie_object(self.queue, 'dr_queue');
+                
                 }
 
                 function find_Item(list, query) {
@@ -155,17 +190,36 @@ angular.module('search-app', ['session-app', 'ngCookies'])
 
                 }
 
-                function b64EncodeUnicode(str) {
-    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
-        return String.fromCharCode('0x' + p1);
-    }));
-}
+                function writeCookie_object(obj, cookie_name) {
+                    var str = b64EncodeUnicode(JSON.stringify(obj));
+                    $cookies.put(cookie_name, str);
+                }
 
-function b64DecodeUnicode(str) {
-    return decodeURIComponent(atob(str).split('').map(function(c) {
-        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
-    }).join(''));
-}
+                function getCookie_object(cookie_name) {
+
+                    var cookie = $cookies.get(cookie_name);
+
+                    if (cookie != null) {
+
+                        cookie = b64DecodeUnicode(cookie);
+
+                        cookie = JSON.parse(cookie);
+                    }
+
+                    return cookie;
+                }
+
+                function b64EncodeUnicode(str) {
+                    return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function (match, p1) {
+                        return String.fromCharCode('0x' + p1);
+                    }));
+                }
+
+                function b64DecodeUnicode(str) {
+                    return decodeURIComponent(atob(str).split('').map(function (c) {
+                        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+                    }).join(''));
+                }
 
             }
 
