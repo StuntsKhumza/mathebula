a194fb4083cedd2fc7eaa230ef7eed6107c5f412
Nkosinathi Khumalo <Nkosinathi.Khumalo@investec.co.za>
2017-04-19 06:26:05 +0200

    Updates - removed console.log

--- configs/navbar.js
+++ configs/navbar.js
@@ -1,61 +1,63 @@
-   var width_limit = 566;
-   
-   var addEvent = function (object, type, callback) {
+var width_limit = 566;
 
-            if (object == null || typeof (object) == 'undefined') return;
+var addEvent = function (object, type, callback) {
 
-            if (object.addEventListener) {
-                object.addEventListener(type, callback, false);
-            } else if (object.attachEvent) {
-                object.attachEvent('on' + type, callback);
-            } else {
-                object['on' + type] = callback;
-            }
+    if (object == null || typeof (object) == 'undefined') return;
 
-
-        };
-
-        addEvent(window, 'resize', test)
-
-        function test() {
-            console.log(window.innerWidth);
-
-            if (window.innerWidth < width_limit) {
-
-                if (getState() == 'block') {
-                    document.getElementById("myTopnav").style.display = "none";
-                }
+    if (object.addEventListener) {
+        object.addEventListener(type, callback, false);
+    } else if (object.attachEvent) {
+        object.attachEvent('on' + type, callback);
+    } else {
+        object['on' + type] = callback;
+    }
 
 
-            } else if (window.innerWidth > width_limit + 1) {
-                if (getState() == 'none') {
-                    document.getElementById("myTopnav").style.display = "block";
-                }
-            }
+};
+
+addEvent(window, 'resize', test)
+
+function test() {
+
+
+    if (window.innerWidth < width_limit) {
+
+        if (getState() == 'block') {
+            document.getElementById("myTopnav").style.display = "none";
         }
 
-        function getState() {
 
-            var state = document.getElementById("myTopnav");
-
-            state = window.getComputedStyle(state).getPropertyValue('display');
-
-            return state;
-
+    } else if (window.innerWidth > width_limit + 1) {
+        if (getState() == 'none') {
+            document.getElementById("myTopnav").style.display = "block";
         }
+    }
+}
 
-        function showNav() {
-            var state = document.getElementById("myTopnav");
+function getState() {
 
-            state = window.getComputedStyle(state).getPropertyValue('display');
+    var state = document.getElementById("myTopnav");
 
-            switch (state) {
-                case "none":
-                    document.getElementById("myTopnav").style.display = "block";
-                    break;
-                case "block":
-                    document.getElementById("myTopnav").style.display = "none";
-                    break;
-            }
+    if (state != null) {
+        state = window.getComputedStyle(state).getPropertyValue('display');
+    } 
 
-        }
+    return state;
+
+}
+
+function showNav() {
+    var state = document.getElementById("myTopnav");
+
+    state = window.getComputedStyle(state).getPropertyValue('display');
+
+    switch (state) {
+        case "none":
+            document.getElementById("myTopnav").style.display = "block";
+            break;
+        case "block":
+            document.getElementById("myTopnav").style.display = "none";
+            break;
+    }
+
+}
